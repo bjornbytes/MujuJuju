d3e9521775b5714611d12fe49a4a59930e531b38
bjorn <tie.372@gmail.com>
2014-08-23 03:10:58 -0600

    Reset minion target on enemy death;

--- app/enemies.lua
+++ app/enemies.lua
@@ -25,5 +25,12 @@ end
 function Enemies:remove(enemy)
 	ctx.view:unregister(enemy)
 	ctx.jujuJuices:add({amount = love.math.random(1, 50), x = enemy.x, y = enemy.y - love.math.random(50, 250), velocity = love.math.random(-1, 1),speed = love.math.random(1, 15)})
+
+	table.each(ctx.minions.minions, function(minion)
+		if minion.target == enemy then
+			minion.target = nil
+		end
+	end)
+
 	self.enemies[enemy] = nil
 end
