385384a617907fd5173d0adb4863a8dcb2a4f020
LloydtheHumble <lloydmcinnes@gmail.com>
2014-08-23 22:15:27 +1200

    Juju Balancing

--- app/enemies.lua
+++ app/enemies.lua
@@ -24,15 +24,15 @@ end
 
 function Enemies:remove(enemy)
 	ctx.view:unregister(enemy)
-	local randomNum = love.math.random(10, 50)
+	local randomNum = love.math.random(10, 35)
 	if randomNum <= 13 then
 		ctx.jujuJuices:add({amount = randomNum, x = enemy.x, y = enemy.y - love.math.random(50, 250), velocity = love.math.random(-1, 1),speed = love.math.random(1, 15)})
 	elseif randomNum > 13 then
-		ctx.jujuJuices:add({amount = math.max(3,randomNum/1.5), x = enemy.x, y = enemy.y - love.math.random(50, 250), velocity = love.math.random(-1, 1),speed = love.math.random(1, 15)})
-		ctx.jujuJuices:add({amount = math.max(3,randomNum/2), x = enemy.x, y = enemy.y - love.math.random(50, 250), velocity = love.math.random(-1, 1),speed = love.math.random(1, 15)})
+		ctx.jujuJuices:add({amount = math.max(7,randomNum/1.5), x = enemy.x, y = enemy.y - love.math.random(50, 250), velocity = love.math.random(-1, 1),speed = love.math.random(1, 15)})
+		ctx.jujuJuices:add({amount = math.max(7,randomNum/2), x = enemy.x, y = enemy.y - love.math.random(50, 250), velocity = love.math.random(-1, 1),speed = love.math.random(1, 15)})
 	end
 	if randomNum > 25 then
-		ctx.jujuJuices:add({amount = math.max(1,randomNum/3), x = enemy.x, y = enemy.y - love.math.random(50, 250), velocity = love.math.random(-1, 1),speed = love.math.random(1, 15)})
+		ctx.jujuJuices:add({amount = math.max(1,randomNum/4), x = enemy.x, y = enemy.y - love.math.random(50, 250), velocity = love.math.random(-1, 1),speed = love.math.random(1, 15)})
 	end
 
 	--ctx.jujuJuices:add({amount = love.math.random(1, 20), x = enemy.x, y = enemy.y - love.math.random(50, 250), velocity = love.math.random(-1, 1),speed = love.math.random(1, 15)})
