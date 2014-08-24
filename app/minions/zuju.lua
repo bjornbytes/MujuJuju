b23a71f911df33aa0f2c949b7fafd669d958460d
bjorn <tie.372@gmail.com>
2014-08-24 05:32:26 -0600

    Improve Zuju animations;

--- app/minions/zuju.lua
+++ app/minions/zuju.lua
@@ -14,7 +14,8 @@ Zuju.maxHealth = 80
 
 function Zuju:init(data)
 	Minion.init(self, data)
-	self.skeleton = Skeleton({name = 'zuju', x = self.x, y = self.y, scale = .5})
+	self.depth = self.depth + love.math.random()
+	self.skeleton = Skeleton({name = 'zuju', x = self.x, y = self.y + self.height, scale = .5})
 
 	self.animator = Animator({
 		skeleton = self.skeleton,
@@ -32,50 +33,59 @@ function Zuju:init(data)
 	self.animator:add(self.animationState, false)
 	self.animator.state.onComplete = function(trackIndex)
 		local name = self.animator.state:getCurrent(trackIndex).animation.name
-		if name == 'spawn' or name == 'cast' then
+		if name == 'spawn' then
 			self.animationLock = nil
+			self.animationState = 'idle'
+			self.animator:set(self.animationState, true)
 		elseif name == 'death' then
 			ctx.minions:remove(self)
 		end
 	end
 
+	self.skeleton.skeleton.flipX = not ctx.player.skeleton.skeleton.flipX
+
 	self.animationSpeeds = table.map({
 		walk = .73 * tickRate,
 		idle = .3 * tickRate,
 		cast = .85 * tickRate,
-		spawn = .45 * tickRate,
+		spawn = .85 * tickRate,
 		death = .8 * tickRate
 	}, f.val)
 end
 
 function Zuju:update()
-	if self.animationState == 'death' then return end
+	if self.animationState == 'death' or self.animationState == 'spawn' then
+		self.animator:update(self.animationSpeeds[self.animationState]())
+		return
+	end
+
 	Minion.update(self)
+
 	if self.target == nil then
 		self.target = ctx.target:getShrine(self)
 	end
 	local dif = self.target.x - self.x
-	if math.abs(dif) > self.attackRange + self.target.width / 2 then
+	local inRange = math.abs(dif) <= self.attackRange + self.target.width / 2
+	if not inRange then
 		self.x = self.x + self.speed * math.sign(dif) * tickRate * self.timeScale
-	elseif self.fireTimer == 0 and self.animationLock == nil then
-		self.animationState = 'cast'
-		self.animationLock = true
-		self.animator:set(self.animationState, false)
 	end
 
 	if not self.animationLock then
-		if math.abs(dif) > self.attackRange + self.target.width / 2 and self.animationState ~= 'walk' then
+		if not inRange and self.animationState ~= 'walk' then
 			self.animationState = 'walk'
 			self.animator:set(self.animationState, true)
-			self.skeleton.skeleton.flipX = dif < 0
-		elseif self.target == ctx.shrine and self.animationState ~= 'idle' then
+		elseif inRange and self.target == ctx.shrine and self.animationState ~= 'idle' then
 			self.animationState = 'idle'
 			self.animator:set(self.animationState, true)
 		end
 	end
 
+	if self.animationState == 'walk' then
+		self.skeleton.skeleton.flipX = dif < 0
+	end
+
 	self.skeleton.skeleton.x = self.x
-	self.skeleton.skeleton.y = self.y + self.height / 2
+	self.skeleton.skeleton.y = self.y + self.height + 8
 	self.animator:update(self.animationSpeeds[self.animationState]())
 end
 
@@ -84,11 +94,13 @@ function Zuju:draw()
 end
 
 function Zuju:hurt(amount)
-	self.health = self.health - amount
+	self.health = math.max(self.health - amount, 0)
 	if self.health <= 0 then
-		self.animationLock = true
-		self.animationState = 'death'
-		self.animator:set('death', false)
+		if self.animationState ~= 'death' then
+			self.animationLock = true
+			self.animationState = 'death'
+			self.animator:set('death', false)
+		end
 		return true
 	end
 end
