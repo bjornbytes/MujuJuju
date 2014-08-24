7fa893a1cd4282e5e3dc4e249dfada2dcee2bf60
bjorn <tie.372@gmail.com>
2014-08-23 22:27:58 -0600

    Fix lightning?;

--- app/particles/lightning.lua
+++ app/particles/lightning.lua
@@ -10,7 +10,10 @@ function Lightning:init(data)
 end
 
 function Lightning:update()
-	self.health = timer.rot(self.health, function() ctx.particles:remove(self) end)
+	self.health = self.health - tickRate
+	if self.health <= 0 then
+		ctx.particles:remove(self)
+	end
 end
 
 function Lightning:draw()
