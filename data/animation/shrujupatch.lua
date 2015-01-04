05bb3479b874eac162d724d31de6e3b4a5d2399b
Sophia <sophiabaldonado@gmail.com>
2015-01-03 23:35:16 -0800

    Shrujupatch Animations;

---
+++ data/animation/shrujupatch.lua
@@ -0,0 +1,19 @@
+local ShrujuPatch = extend(Animation)
+ShrujuPatch.code = 'shrujupatch'
+
+ShrujuPatch.scale = 1
+ShrujuPatch.default = 'spawn'
+ShrujuPatch.states = {}
+
+ShrujuPatch.states.spawn = {
+  priority = 1,
+  speed = .85
+}
+
+ShrujuPatch.states.idle = {
+  priority = 1,
+  loop = true,
+  speed = .06
+}
+
+return ShrujuPatch
