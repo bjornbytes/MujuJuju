7831148a7ffe6af22c2fa4d8e61a0b891eb866d2
bjorn <tie.372@gmail.com>
2015-01-04 12:16:09 -0700

    Tweak grass;

--- app/map.lua
+++ app/map.lua
@@ -37,7 +37,7 @@ function Map:init()
     draw = function()
       local image = data.media.graphics.grass
       local scale = self.height / data.media.graphics.map[ctx.biome]:getHeight()
-      local shearx = math.sin(tick / 100) * math.cos(tick / 50) * .08
+      local shearx = math.sin(tick / 100) * math.cos(tick / 60) ^ 2 * .08
       g.setColor(200, 200, 200)
       g.draw(image, self.width / 2, self.height, 0, scale, scale, image:getWidth() / 2, image:getHeight(), shearx)
       local alpha = self.spiritAlpha * 255
