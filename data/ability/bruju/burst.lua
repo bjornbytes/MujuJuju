e8cf5b88e2f2a81d624c47554922da4eb8cec7c4
bjorn <tie.372@gmail.com>
2015-01-07 15:18:44 -0700

    Fix sanctuary bug;

--- data/ability/bruju/burst.lua
+++ data/ability/bruju/burst.lua
@@ -4,7 +4,7 @@ Burst.code = 'burst'
 function Burst:die()
   local damage = 25 * self.unit:upgradeLevel('burst')
   local range = 85 + (6 * self.unit:upgradeLevel('burst'))
-  local heal = 10 + 5 * self.unit:upgradeLevel('sanctuary')
+  local heal = self.unit:upgradeLevel('sanctuary') > 0 and 10 + 5 * self.unit:upgradeLevel('sanctuary') or 0
 
   self:createSpell({
     damage = damage,
