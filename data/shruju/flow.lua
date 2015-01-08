3b0ac387127f403fec6fe71ec565950a0e84dd66
bjorn <tie.372@gmail.com>
2015-01-08 00:07:02 -0700

    Slightly more enemies; Buff juju + flow shruju;

--- data/shruju/flow.lua
+++ data/shruju/flow.lua
@@ -2,11 +2,11 @@ local Flow = extend(Shruju)
 
 Flow.code = 'flow'
 Flow.name = 'Flow'
-Flow.description = 'The cooldown for minion summing is reduced by .25s.'
+Flow.description = 'The cooldown for minion summoning is reduced by .5s.'
 
 function Flow:apply()
   local p = ctx.players:get(ctx.id)
-  p.flatCooldownReduction = p.flatCooldownReduction + .25
+  p.flatCooldownReduction = p.flatCooldownReduction + .5
 
   if config.player.baseCooldown - p.flatCooldownReduction <= config.player.minCooldown then
     table.each(ctx.shrujuPatches.objects, function(patch)
@@ -15,7 +15,7 @@ function Flow:apply()
   end
 
   ctx.spells:add('arcadetext', {
-    text = '.25s faster cooldown',
+    text = '.5s faster cooldown',
     x = p.x,
     y = p.y - 40
   })
--- data/shruju/juju.lua
+++ data/shruju/juju.lua
@@ -2,16 +2,16 @@ local Juju = extend(Shruju)
 
 Juju.code = 'juju'
 Juju.name = 'Juju'
-Juju.description = 'On use, gives 50 juju.'
+Juju.description = 'On use, gives 50 juju (+5 per minute).'
 
 function Juju:apply()
   local p = ctx.players:get(ctx.id)
-  local amount = 50
+  local amount = 50 + 5 * math.floor(ctx.timer * tickRate / 60)
   p.juju = p.juju + amount
   ctx.hud.status.jujuScale = 2
   ctx.hud.status.jujuAngle = 0
   ctx.spells:add('arcadetext', {
-    text = '+50 juju',
+    text = '+' .. amount .. ' juju',
     x = p.x,
     y = p.y - 40
   })
