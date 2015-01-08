a2df6a893cfb1a03fc66e7867e9e9da4785bfa7f
bjorn <tie.372@gmail.com>
2015-01-08 04:55:58 -0700

    Fix flow;

--- data/shruju/flow.lua
+++ data/shruju/flow.lua
@@ -8,7 +8,7 @@ function Flow:apply()
   local p = ctx.players:get(ctx.id)
   p.flatCooldownReduction = p.flatCooldownReduction + .5
 
-  if config.player.baseCooldown - p.flatCooldownReduction <= config.biomes[ctx.biome].player.minCooldown then
+  if config.biomes[ctx.biome].player.baseCooldown - p.flatCooldownReduction <= config.biomes[ctx.biome].player.minCooldown then
     table.each(ctx.shrujuPatches.objects, function(patch)
       patch:removeType('flow')
     end)
