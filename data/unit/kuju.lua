c7d605b3820b83db4b4fbe4c98a9893e4e226c7d
bjorn <tie.372@gmail.com>
2015-01-09 04:17:16 -0700

    Kuju is a carry;

--- data/unit/kuju.lua
+++ data/unit/kuju.lua
@@ -7,13 +7,13 @@ Kuju.description = 'Yay Kuju.'
 -- Stats
 ----------------
 Kuju.health = 60
-Kuju.damage = 15
-Kuju.range = 140
+Kuju.damage = 10
+Kuju.range = 145
 Kuju.attackSpeed = 1.5
 Kuju.speed = 30
 
 Kuju.healthScaling = {6, 1}
-Kuju.damageScaling = {.8, .95}
+Kuju.damageScaling = {.4, 1.15}
 
 Kuju.attackSpell = 'kujuattack'
 
