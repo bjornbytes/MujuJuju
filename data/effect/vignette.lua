e72516126788d8c1c45cd81c3c13a0cd7911493f
bjorn <tie.372@gmail.com>
2015-01-09 22:34:22 -0700

    Vignette;

--- data/effect/vignette.lua
+++ data/effect/vignette.lua
@@ -12,8 +12,8 @@ function Vignette:update()
   local p = ctx.player
 	self.blur = math.lerp(self.blur, p.dead and self.config.blur[2] or self.config.blur[1], 2 * tickRate)
 	self.radius = math.lerp(self.radius, p.dead and self.config.radius[2] or self.config.radius[1], 4 * tickRate)
-	self.shader:send('blur', self.blur)
-	self.shader:send('radius', self.radius)
+	--self.shader:send('blur', self.blur)
+	--self.shader:send('radius', self.radius)
 end
 
 function Vignette:resize()
