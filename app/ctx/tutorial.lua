68b5960dedd4465fa6682033db13ef88c41030c2
bjorn <tie.372@gmail.com>
2015-02-10 15:40:21 -0700

    This tutorial effect is sort of okay;

--- app/ctx/tutorial.lua
+++ app/ctx/tutorial.lua
@@ -1,19 +1,44 @@
+local popo = require 'lib/deps/popo/Text'
+local g = love.graphics
 Tutorial = class()
 
-local function goToGame()
-	Context:remove(ctx)
-	Context:add(Game)
-end
-
 function Tutorial:init()
-	self.bg = love.graphics.newImage('media/graphics/tutorial.png')
-end
+  self.text = popo('[Welcome to Muju Juju!](ease)', {
+    font = g.setFont('mesmerize', .1 * love.graphics.getHeight()),
 
-Tutorial.keypressed = goToGame
-Tutorial.mousepressed = goToGame
-Tutorial.gamepadpressed = goToGame
+    easeInit = function(c)
+      c.t = 0
+      c.targetx = c.x
+      c.targety = c.y
+    end,
+
+    ease = function(dt, c)
+      c.t = c.t + dt
+      local direction = c.position
+      local threshold = math.clamp((c.t - (.02 * c.position)) / .4, 0, 1) ^ 2
+      local distance = 50 * (1 - threshold)
+      local alpha = threshold ^ 2
+      g.setColor(255, 255, 255, 255 * alpha)
+      c.x = c.targetx + math.dx(distance, direction)
+      c.y = c.targety + math.dy(distance, direction)
+    end
+  })
+end
 
 function Tutorial:draw()
-	love.graphics.setColor(255, 255, 255)
-	love.graphics.draw(self.bg)
+  local u, v = love.graphics.getDimensions()
+  local font = self.text.font
+
+  g.setColor(0, 0, 0)
+  g.rectangle('fill', 0, 0, u, v)
+
+  g.setColor(255, 255, 255)
+  self.text:update(delta)
+  self.text:draw(u * .5 - font:getWidth(self.text.str_text) / 2, v * .5)
+end
+
+function Tutorial:keypressed(key)
+  table.each(self.text.characters, function(c)
+    c.t = 0
+  end)
 end
