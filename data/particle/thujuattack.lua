b1f15e2ac14e68962e0958e82c2945a44d879a5e
bjorn <tie.372@gmail.com>
2015-02-16 05:26:40 -0700

    Thuju attack particle;

---
+++ data/particle/thujuattack.lua
@@ -0,0 +1,14 @@
+local ThujuAttack = class()
+ThujuAttack.image = data.media.graphics.particles.smoke
+ThujuAttack.max = 256
+ThujuAttack.blendMode = 'additive'
+
+ThujuAttack.options = {}
+ThujuAttack.options.particleLifetime = {.5}
+ThujuAttack.options.colors = {{150, 130, 100, 100}, {150, 130, 200, 0}}
+ThujuAttack.options.sizes = {.6, 1}
+ThujuAttack.options.sizeVariation = .1
+ThujuAttack.options.areaSpread = {'normal', 6, 6}
+ThujuAttack.options.rotation = {0, 2 * math.pi}
+
+return ThujuAttack
