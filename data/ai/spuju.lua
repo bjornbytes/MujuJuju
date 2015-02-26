30a6005300e4ec24f4bd4cf31b3e42467bbafde2
bjorn <tie.372@gmail.com>
2015-02-26 16:17:57 -0700

    Wahhh;

--- data/ai/spuju.lua
+++ data/ai/spuju.lua
@@ -1,14 +1,8 @@
 local Spuju = extend(UnitAI)
 
 function Spuju:update()
-  if ctx.player.dead then
-    self.unit.untargetable = true
-    self.unit.alpha = math.lerp(self.unit.alpha, 0, math.min(10 * ls.tickrate, 1))
-  else
-    self.unit.untargetable = false
-    self.unit.alpha = math.lerp(self.unit.alpha, 1, math.min(10 * ls.tickrate, 1))
-  end
-
+  -- Nothing for now
+  
   UnitAI.update(self)
 end
 
