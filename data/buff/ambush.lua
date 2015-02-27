e459730b7a3255df5c6b50fa6b57d95a64577a48
bjorn <tie.372@gmail.com>
2015-02-26 18:42:37 -0700

    Fix ambush crash;

--- data/buff/ambush.lua
+++ data/buff/ambush.lua
@@ -4,7 +4,7 @@ Ambush.tags = {}
 function Ambush:activate()
   self.unit.animation:set('vanish', {force = true})
   self.unit.animation:on('event', function(event)
-    if event.data.name == 'vanished' then
+    if event.data.name == 'vanish' then
       self.timer = 3
       self.unit.visible = false
     end
@@ -13,8 +13,10 @@ end
 
 function Ambush:deactivate()
   local target = ctx.target:closest(self.unit, 'enemy', 'unit')
-  self.unit.x = target.x + (target.animation.flipped and -1 or 1) * (self.unit.width / 2)
-  self.unit.visible = true
+  if target then
+    self.unit.x = target.x + (target.animation.flipped and -1 or 1) * (self.unit.width / 2)
+    self.unit.visible = true
+  end
 end
 
 return Ambush
