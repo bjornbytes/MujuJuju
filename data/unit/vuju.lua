ef4f2b258e25bec56171570cbb94756142f8fde4
bjorn <tie.372@gmail.com>
2015-02-28 22:08:20 -0700

    Vuju is slightly more vulnerable;

--- data/unit/vuju.lua
+++ data/unit/vuju.lua
@@ -8,10 +8,10 @@ Vuju.description = 'Yay Vuju.'
 Vuju.health = 65
 Vuju.damage = 8
 Vuju.range = 125
-Vuju.attackSpeed = 1.4
+Vuju.attackSpeed = 1.5
 Vuju.speed = 40
 
-Vuju.healthScaling = {7, 1}
+Vuju.healthScaling = {6, 1}
 Vuju.damageScaling = {.5, 1.0}
 
 Vuju.attackSpell = 'vujuattack'
