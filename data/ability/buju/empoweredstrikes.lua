cc22228b1eec004e2e153e0fc2d5f28e316e9b71
bjorn <tie.372@gmail.com>
2015-02-28 21:43:51 -0700

    Remove print;

--- data/ability/buju/empoweredstrikes.lua
+++ data/ability/buju/empoweredstrikes.lua
@@ -7,7 +7,6 @@ function EmpoweredStrikes:postattack()
   if buff then buff.maxStacks = 1 + self.unit:upgradeLevel('empoweredstrikes') end
   buff = self.unit.buffs:add('empoweredstrikes', {timer = 5})
   buff.frenzy = buff.stack and amount * buff.stacks or amount
-  print(buff.stack, buff.stacks, amount, buff.frenzy)
 end
 
 return EmpoweredStrikes
