7d9748c9479090fbc71fbe54d70dc7b0ed994ed3
Trey Cordova <treycordova@hotmail.com>
2015-03-01 18:20:03 -0800

    You can now remove user accounts;

--- app/menu/menuuser.lua
+++ app/menu/menuuser.lua
@@ -15,6 +15,14 @@ function MenuUser:init()
       return {u * .5 - w / 2, v * .5 - (h * 1.5), w, h}
     end,
 
+    remove1 = function()
+      local slot1 = self.geometry.slot1
+      local u, v = ctx.u, ctx.v
+      local w = u * .04
+      local h = v * .06
+      return {slot1[1] + slot1[3] + 5, slot1[2], w, h}
+    end,
+
     slot2 = function()
       local u, v = ctx.u, ctx.v
       local sx, sy, sw, sh = unpack(self.geometry.slot1)
@@ -23,12 +31,28 @@ function MenuUser:init()
       return {u * .5 - w / 2, sy + sh + 5, w, h}
     end,
 
+    remove2 = function()
+      local slot2 = self.geometry.slot2
+      local u, v = ctx.u, ctx.v
+      local w = u * .04
+      local h = v * .06
+      return {slot2[1] + slot2[3] + 5, slot2[2], w, h}
+    end,
+
     slot3 = function()
       local u, v = ctx.u, ctx.v
       local sx, sy, sw, sh = unpack(self.geometry.slot2)
       local w = u * .45
       local h = v * .15
       return {u * .5 - w /2, sy + sh + 5, w, h}
+    end,
+
+    remove3 = function()
+      local slot3 = self.geometry.slot3
+      local u, v = ctx.u, ctx.v
+      local w = u * .04
+      local h = v * .06
+      return {slot3[1] + slot3[3] + 5, slot3[2], w, h}
     end
   }
 
@@ -51,12 +75,20 @@ function MenuUser:activate()
 
   -- Initialize slots and their states
   self.slots = {}
+  self.remove = {}
   for i = 1, 3 do
     self.slots[i] = ctx.gooey:add(Button, 'menu.user.slot' .. i)
     self.slots[i].geometry = function() return self.geometry['slot' .. i] end
     self.slots[i].text = self.users[i] and self.users[i].name or 'Empty Slot'
     self.slots[i].empty = not self.users[i]
     self.slots[i]:on('click', function() self:slotPicked(i) end)
+
+    if self.users[i] then
+      self.remove[i] = ctx.gooey:add(Button, 'menu.user.remove' .. i)
+      self.remove[i].geometry = function() return self.geometry['remove' .. i] end
+      self.remove[i].text = 'X'
+      self.remove[i]:on('click', function() self:removeSlot(i) end)
+    end
   end
 end
 
@@ -75,6 +107,7 @@ function MenuUser:draw()
 
   for i = 1, #self.slots do
     self.slots[i]:draw()
+    if self.remove[i] then self.remove[i]:draw() end
   end
 end
 
@@ -96,5 +129,15 @@ function MenuUser:slotPicked(slot)
   else
     ctx.choose.user.slot = slot
     ctx:goto('choose')
+  end
+end
+
+function MenuUser:removeSlot(slot)
+  print(slot, self.slots[slot].empty)
+  if not self.slots[slot].empty then
+    love.filesystem.remove('save/' .. self.users[slot].name .. '/user.json')
+    love.filesystem.remove('save/' .. self.users[slot].name .. '/achievements.json')
+    love.filesystem.remove('save/' .. self.users[slot].name)
+    ctx:goto('select')
   end
 end
