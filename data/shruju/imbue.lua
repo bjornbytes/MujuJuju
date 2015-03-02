aae007a4b5f36e394ac44d593a235839533085b5
bjorn <tie.372@gmail.com>
2015-03-02 15:25:51 -0700

    Imbue nerf;

--- data/shruju/imbue.lua
+++ data/shruju/imbue.lua
@@ -1,14 +1,14 @@
 local Imbue = extend(Shruju)
 
 Imbue.name = 'Imbue'
-Imbue.description = 'The shrine heals 0.5% of its maximum health every second.'
+Imbue.description = 'The shrine heals .4% of its maximum health every second.'
 
 function Imbue:apply()
-  ctx.shrine.regen = ctx.shrine.regen + 25
+  ctx.shrine.regen = ctx.shrine.regen + (ctx.shrine.maxHealth * .004)
 end
 
 function Imbue:remove()
-  ctx.shrine.regen = ctx.shrine.regen - 25
+  ctx.shrine.regen = ctx.shrine.regen - (ctx.shrine.maxHealth * .004)
 end
 
 return Imbue
