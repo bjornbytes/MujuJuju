98ca0ffc1709af6e050f25be2a5262a2fc31ca6c
bjorn <tie.372@gmail.com>
2015-03-04 19:44:33 -0700

    Fix puppetize bug;

--- data/buff/puppetize.lua
+++ data/buff/puppetize.lua
@@ -2,6 +2,7 @@ local Puppetize = extend(Buff)
 Puppetize.tags = {}
 
 function Puppetize:activate()
+  self.unit.channeling, self.unit.spawning, self.unit.casting = false, false, false
   self.unit.team = self.unit.team == 0 and ctx.player.team or 0
   self.unit.target = nil
   self.unit.animation:set('idle', {force = true})
