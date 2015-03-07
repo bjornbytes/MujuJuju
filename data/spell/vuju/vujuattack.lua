9d993c0cdfd7ceb222ebf8f92c264577f82261d3
bjorn <tie.372@gmail.com>
2015-03-07 04:10:02 -0700

    Fix vuju attack particle;

--- data/spell/vuju/vujuattack.lua
+++ data/spell/vuju/vujuattack.lua
@@ -1,11 +1,11 @@
 local VujuAttack = extend(Spell)
 
 function VujuAttack:activate()
-  ctx.particles:emit('vujuattack', self.target.x, self.target.y - self.target.height / 2, 12)
   if isa(self.target, Unit) then
-    self.target.buffs:add('vujuattackdot', {timer = 4, dot = self.unit.damage})
+    ctx.particles:emit('vujuattack', self.target.x, self.target.y - self.target.height / 2, 12)
+    self.target.buffs:add('vujuattackdot', {timer = 4, dot = self.unit.damage, noparticles = true})
   else
-    self.unit:attack({target = self.target, damage = self.unit.damage})
+    self.unit:attack({target = self.target, damage = self.unit.damage, noparticles = true})
   end
   ctx.spells:remove(self)
 end
