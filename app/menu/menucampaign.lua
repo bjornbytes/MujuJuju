444564de3775f034844290e4f45dedc57bfa1616
bjorn <tie.372@gmail.com>
2015-03-06 21:11:25 -0700

    Bigger hats;

--- app/menu/menucampaign.lua
+++ app/menu/menucampaign.lua
@@ -203,7 +203,7 @@ function MenuCampaign:draw()
       g.draw(atlas.texture, atlas.quads.runeBgBroken, medalX, medalY + .14 * v, 0, scale, scale, qw / 2, qh / 2)
     elseif benchmark == 'silver' then
       local image = ctx.animations.muju.graphics.santa
-      local scale = medalSize * 1.5 / image:getWidth()
+      local scale = medalSize * 2 / image:getWidth()
       g.setColor(achieved and {255, 255, 255} or {0, 0, 0})
       g.draw(image, medalX, medalY + .14 * v, 0, scale, scale, image:getWidth() / 2, image:getHeight() / 2)
     elseif benchmark == 'gold' then
