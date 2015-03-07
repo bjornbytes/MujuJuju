240ae4bba38d7cd74b79472c86004a3cb7a19a32
bjorn <tie.372@gmail.com>
2015-03-07 04:05:40 -0700

    More kuju particles;

--- data/buff/shiverarmor.lua
+++ data/buff/shiverarmor.lua
@@ -12,7 +12,9 @@ function ShiverArmor:prehurt(amount, source, kind)
 end
 
 function ShiverArmor:update()
-  ctx.particles:emit('magicshruju', self.player.x, self.player.y, 1)
+  if love.math.random() < .8 then
+    ctx.particles:emit('kujuattack', self.player.x, self.player.y + self.player.height / 2, 1)
+  end
 end
 
 function ShiverArmor:die()
@@ -20,6 +22,7 @@ function ShiverArmor:die()
     local targets = ctx.target:inRange(ctx.player, 180, 'enemy', 'unit')
     table.each(targets, function(target)
       target:hurt(self.damage, self.player)
+      ctx.particles:emit('frozenorb', target.x, target.y, 1)
 
       if target.buffs and love.math.random() < self.stunChance then
         target.buffs:add('crystallize', {timer = self.stunDuration})
